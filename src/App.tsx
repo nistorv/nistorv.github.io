9c76609bcfbedda7ee42d1be8e92c776e0ed8d61
Vlad Nistor <nistorv06@gmail.com>
2025-06-26 19:07:43 +1200

    added subtitle and slight design adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import { IconButton } from "./components/IconButton";
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-slate-100">
-      <div className="bg-white px-5 py-10 p-10 rounded-xl shadow-md">
-        <h1 className="text-4xl mx-10">Vlad Nistor</h1>
-        <div className="flex flex-row justify-center divide-x divide-slate-600 py-2">
+      <div className="bg-white px-5 p-10 rounded-xl shadow-md">
+        <h1 className="text-4xl mx-10 text-center">Vlad Nistor</h1>
+        <p className="max-w-2xl flex text-center mx-5 py-2">
+          {" "}
+          Second-year computer science student @ University of Canterbury
+        </p>
+        <div className="flex flex-row justify-center divide-x divide-slate-600">
           <IconButton href="https://www.linkedin.com/in/nistorv/">
             <FaLinkedin size={24} />
           </IconButton>
