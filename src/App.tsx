1ddcd3b4b68f4e66bedbd098b2500276ec036dda
Vlad Nistor <nistorv06@gmail.com>
2025-06-26 19:52:07 +1200

    added bg + minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 import { IconButton } from "./components/IconButton";
 
+import backgroundImage from "./assets/background.jpg";
+
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-100">
+    <div
+      className={`w-screen h-screen flex justify-center items-center`}
+      style={{ background: `url(${backgroundImage})` }}
+    >
       <div className="bg-white px-5 p-10 rounded-xl shadow-md">
         <h1 className="text-4xl mx-10 text-center">Vlad Nistor</h1>
         <p className="max-w-2xl flex text-center mx-5 py-2">
-          {" "}
           Second-year computer science student @ University of Canterbury
         </p>
         <div className="flex flex-row justify-center divide-x divide-slate-600">
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -5,7 +5,10 @@ export interface IconButtonProps {
 
 export function IconButton(props: IconButtonProps) {
   return (
-    <a className="flex justify-center items-center px-4 py-2" href={props.href}>
+    <a
+      className="flex justify-center items-center px-4 py-2 text-slate-800 hover:bg-slate-800 hover:text-white"
+      href={props.href}
+    >
       {props.children}
     </a>
   );
