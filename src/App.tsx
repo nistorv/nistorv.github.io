a768378259302231306cdc350199c11c4e778c88
nistorv <nistorv06@gmail.com>
2025-07-30 12:17:59 +1200

    fix to bg + git ignore addition

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import backgroundImage from "./assets/background.jpg";
 function App() {
   return (
     <div
-      className={`w-screen h-screen flex justify-center items-center`}
-      style={{ background: `url(${backgroundImage})` }}
+      className="w-screen h-screen flex justify-center items-center"
+      style={{
+        background: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+      }}
     >
-      <div className="bg-white px-5 p-10 rounded-xl shadow-md">
+      <div className="bg-white px-5 p-10 rounded-xl shadow-2xl">
         <h1 className="text-4xl mx-10 text-center">Vlad Nistor</h1>
         <p className="max-w-2xl flex text-center mx-5 py-2">
           Second-year computer science student @ University of Canterbury
