0b753434c843f3a75eb9bfd275155c674faa99cb
nistorv <nistorv06@gmail.com>
2026-07-27 15:13:04 +1200

    fix: fixed location url not working + content: updated photos and project weightings

--- src/components/BackgroundDescriptor.tsx
+++ src/components/BackgroundDescriptor.tsx
@@ -10,7 +10,7 @@ export function BackgroundDescriptor(props: BackgroundDescriptorProps) {
     <div className="absolute bottom-4 right-4 text-sm text-white/75">
       <a
         className="flex items-center gap-2 hover:text-white"
-        href={`www.google.com/maps?q=loc:${props.background.lat},${props.background.lon}`}
+        href={`https://www.google.com/maps?q=loc:${props.background.lat},${props.background.lon}`}
       >
         <FaLocationDot />
         <span>{props.background.name}</span>
